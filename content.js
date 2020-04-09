1408ba90e7aa1034a4e4d93123035504163432c2
Justin Rodriguez <rodriguezjustin657@gmail.com>
2020-04-09 15:24:30 -0500

    eliminates ads from yahoo news and huffington post

--- content.js
+++ content.js
@@ -1,7 +1,38 @@
 console.log("Boom Awesome?");
 
-let paragraphs = document.getElementsByTagName('p');
-for (elt of paragraphs) {
+// let paragraphs = document.getElementsByTagName('p');
+// for (elt of paragraphs) {
+//     console.log(elt);
+//     elt.style['font-size'] = '20pt';
+// }
+
+let body = document.getElementsByTagName('body');
+for (elt of body) {
     console.log(elt);
-    elt.style['font-size'] = '100pt';
+    elt.style['background-color'] = '#FFFFFF';
+}
+
+let ads = document.getElementsByClassName('adsbygoogle');
+for (elt of ads) {
+    elt.style['opacity'] = '0.0';
+}
+
+let adwrappers = document.getElementsByClassName('ad_wrapper_');
+for (elt of adwrappers) {
+    elt.style['opacity'] = '0.0';
+}
+
+let right_rail = document.getElementsByClassName('right-rail');
+for (elt of right_rail) {
+    elt.style['opacity'] = '0.0';
+}
+
+let advertisement = document.getElementsByClassName('advertisement');
+for (elt of advertisement) {
+    elt.style['opacity'] = '0.0';
+}
+
+let YahooNewsAds = document.getElementsByClassName('YDC-Sidekick-Wrapper');
+for (elt of YahooNewsAds) {
+    elt.style['opacity'] = '0.0';
 }
